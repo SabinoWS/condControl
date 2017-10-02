7f422265e4fbff336a94c7fe42bf96ad7130762b
SabinoWS <sabino.th@hotmail.com>
2017-10-02 17:29:52 -0300

    controller

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 class AdministratorController extends Controller
 {
     public function index(){
-
-
         return view('administrator.index');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,17 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->group(function () {
-    Route::get('painel', )->name('admin-panel');
+    Route::get('painel', 'AdministratorController@index')->name('admin-panel');
+});
+
+Route::prefix('manager')->group(function () {
+    Route::get('painel', 'ManagerController@index')->name('admin-panel');
+});
+
+Route::prefix('host')->group(function () {
+    Route::get('painel', 'HostController@index')->name('admin-panel');
+});
+
+Route::prefix('resident')->group(function () {
+    Route::get('painel', 'ResidentController@index')->name('admin-panel');
 });
