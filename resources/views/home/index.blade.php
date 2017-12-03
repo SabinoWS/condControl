c6f681faf4d83ca8be621ff59c342c105bfb9d2c
Taugustos <tiago.rs@hotmail.com>
2017-12-03 17:51:01 -0200

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -149,7 +149,7 @@ http://www.templatemo.com/tm-406-flex
                             <div class="service-content">
                                 <div class="inner-service">
                                    <h3>Chat</h3>
-                                   <p>Canal de Interação Exclusivo entre os condominos e o síndico.</p>
+                                   <p>Canal de Interação Exclusivo entre os condôminos e o síndico.</p>
                                 </div>
                             </div> <!-- /.service-content -->
                         </div> <!-- /#service-1 -->
