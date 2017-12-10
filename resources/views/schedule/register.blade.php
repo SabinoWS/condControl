6a07e132f42774b2b154b2443f0a309d30cc8df5
SabinoWS <sabino.th@hotmail.com>
2017-12-10 03:24:15 -0200

    Ajustando views

--- resources/views/schedule/register.blade.php
+++ resources/views/schedule/register.blade.php
@@ -5,26 +5,14 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="main-panel panel panel-default">
-                <div class="panel-heading">Registrar Local</div>
+                <div class="panel-heading">Registrar Horário</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('save-local') }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('save-schedule') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Nome</label>
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" autofocus>
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
                         <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-                            <label for="description" class="col-md-4 control-label">Descrição</label>
+                            <label for="description" class="col-md-4 control-label">Observações (Opcional)</label>
                             <div class="col-md-6">
                                 <input id="description" type="text" class="form-control" name="description" value="{{ old('description') }}" autofocus>
                                 @if ($errors->has('description'))
@@ -36,9 +24,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('capacity') ? ' has-error' : '' }}">
-                            <label for="capacity" class="col-md-4 control-label">Capacidade</label>
+                            <label for="capacity" class="col-md-4 control-label">Data</label>
                             <div class="col-md-6">
-                                <input id="capacity" type="text" class="form-control" name="capacity" value="{{ old('capacity') }}" autofocus>
+                                <input id="capacity" type="date" class="form-control" name="capacity" value="{{ old('capacity') }}" autofocus>
                                 @if ($errors->has('capacity'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('capacity') }}</strong>
