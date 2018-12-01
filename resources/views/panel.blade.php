1b00890c1007843f9bd8aab00e14b56918f1449f
Sabino <sabino.th@hotmail.com>
2018-12-01 15:27:53 -0200

    Ajustes em botoes

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -14,7 +14,11 @@
 
                 @permission('resident-area')
                     @if (auth()->getUser()->getHolders())
-                        Responsável: {{ auth()->getUser()->getHolders()->getName() }}
+                    <div class="row">
+                        <div class="col-xs-12">
+                            &nbsp&nbsp&nbsp<strong>Responsável:</strong> {{ auth()->getUser()->getHolders()->getName() }}
+                        </div>
+                    </div>
                     @endif
                 @endpermission
 
@@ -24,49 +28,85 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    @permission('admin-area')
-                    <div class="row col-sm-offset-4">
-                        <div class="col-xs-12 col-sm-3 text-center">
-                            <a title="Gerenciar Condomínios" href="{{ route('management-condominium') }}" class="no-style">
-                                <i class="main-panel-button fa fa-building-o btn btn-default"></i>
-                                <div>Condomínios</div>
-                            </a>
+                    {{-- BOTOES DE NAV --}}
+                    <div style="" class="text-center"> 
+
+                        @permission('admin-area')
+                        <div class="row col-sm-offset-4">
+                            <div class="col-xs-12 col-sm-3 text-center">
+                                <a title="Gerenciar Condomínios" href="{{ route('management-condominium') }}" class="no-style">
+                                    <i class="main-panel-button fa fa-building-o btn btn-default"></i>
+                                    <div>Condomínios</div>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-sm-3 text-center">
+                                <a title="Gerenciar Síndicos" href="{{ route('create-manager') }}" class="no-style">
+                                    <i class="fa fa-id-badge main-panel-button btn btn-default"></i>
+                                    <div>Síndicos</div>
+                                </a>
+                            </div>
                         </div>
-                        <div class="col-xs-12 col-sm-3 text-center">
-                            <a title="Gerenciar Síndicos" href="{{ route('create-manager') }}" class="no-style">
-                                <i class="fa fa-id-badge main-panel-button btn btn-default"></i>
-                                <div>Síndicos</div>
-                            </a>
+                        @endpermission
+
+                        @permission('manager-area')
+                        <div class="row">
+                            <div style="display: inline;" class="text-left col-md-4">
+                                <a title="Administrar Proprietário" href="{{ route('management-holder') }}" class="no-style">
+                                    <i class="fa fa-key main-panel-button btn btn-default"></i>
+                                </a>
+                                Administrar Proprietário
+                            </div>
+                            <div style="display: inline;" class="text-left col-md-4">
+                                <a title="Administrar Notícias" href="{{ route('management-news') }}" class="no-style">
+                                    <i class="fa fa-newspaper-o main-panel-button btn btn-default" aria-hidden="true"></i>
+                                </a>
+                                Administrar Notícias
+                            </div>
+                            <div style="display: inline;" class="text-left col-md-4">
+                                <a title="Administrar Locais" href="{{ route('management-local') }}" class="no-style">
+                                    <i class="fa fa-map-marker main-panel-button btn btn-default" aria-hidden="true">&nbsp</i>
+                                </a>
+                                Administrar Locais
+                            </div>
                         </div>
+                        @endpermission
+
+                        @permission('holder-area')
+                        <div class="row">
+                        <div style="display: inline;" class="text-left col-md-4">
+                            <a title="Administrar Moradores" href="{{ route('management-resident') }}" class="no-style">
+                                <i class="fa fa-home main-panel-button btn btn-default"></i>
+                            </a>
+                            Administrar Moradores
+                        </div>
+                        </div>
+                        @endpermission
+
+                        @permission('resident-area')
+                        <div class="row">
+                            <div style="display: inline;" class="text-left col-md-4">
+                                <a title="Mensagens" href="{{ route('chat-board') }}" class="no-style">
+                                    <i class="fa fa-envelope-o main-panel-button btn btn-default"></i>
+                                </a>
+                                Chat
+                            </div>
+                            <div style="display: inline;" class="text-left col-md-4">
+                                <a title="Galeria" href="{{ route('galery-board') }}" class="no-style">
+                                    <i class="fa fa-camera-retro main-panel-button btn btn-default">&nbsp</i>
+                                </a>
+                                Galeria
+                            </div>
+                            <div style="display: inline;" class="text-left col-md-4">
+                                <a title="Reservar Local" href="{{ route('choose-local') }}" class="no-style">
+                                    <i class="fa fa-calendar main-panel-button btn btn-default"></i>
+                                </a>
+                                Reservar Local
+                            </div>
+                        </div>
+                        @endpermission
+                        <br><br>
                     </div>
-                    @endpermission
-                    @permission('manager-area')
-                        <a title="Administrar Proprietário" href="{{ route('management-holder') }}" class="no-style">
-                            <i class="fa fa-key main-panel-button btn btn-default"></i>
-                        </a>
-                        <a title="Administrar Notícias" href="{{ route('management-news') }}" class="no-style">
-                            <i class="fa fa-newspaper-o main-panel-button btn btn-default" aria-hidden="true"></i>
-                        </a>
-                        <a title="Administrar Locais" href="{{ route('management-local') }}" class="no-style">
-                            <i class="fa fa-map-marker main-panel-button btn btn-default" aria-hidden="true"></i>
-                        </a>
-                    @endpermission
-                    @permission('holder-area')
-                        <a title="Administrar Moradores" href="{{ route('management-resident') }}" class="no-style">
-                            <i class="fa fa-home main-panel-button btn btn-default"></i>
-                        </a>
-                    @endpermission
-                    @permission('resident-area')
-                        <a title="Mensagens" href="{{ route('chat-board') }}" class="no-style">
-                            <i class="fa fa-envelope-o main-panel-button btn btn-default"></i>
-                        </a>
-                        <a title="Galeria" href="{{ route('galery-board') }}" class="no-style">
-                            <i class="fa fa-camera-retro main-panel-button btn btn-default"></i>
-                        </a>
-                        <a title="Reservar Local" href="{{ route('choose-local') }}" class="no-style">
-                            <i class="fa fa-calendar main-panel-button btn btn-default"></i>
-                        </a>
-                    @endpermission
+                    {{-- FIM BOTOES DE NAV --}}
 
                     @forelse ($news as $new)
                         <div class="panel panel-default news-panel">
